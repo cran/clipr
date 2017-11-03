41ffae88485cf2c3bf2824a6d6a9b195a562f467
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-11-03 11:20:02 +0000

    version 0.4.0

--- tests/testthat/test_render.R
+++ tests/testthat/test_render.R
@@ -8,12 +8,21 @@ test_that("clipr_available fails when DISPLAY is not configured; succeeds when i
   skip_if_not(identical(Sys.getenv("TRAVIS"), "true"))
   if (identical(Sys.getenv("TRAVIS_CLIP"), "none"))
     expect_false(is_clipr_available)
-  if (identical(Sys.getenv("TRAVIS_CLIP"), "xclip")) {
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xclip"))
     expect_true(is_clipr_available)
-  }
-  if (identical(Sys.getenv("TRAVIS_CLIP"), "xsel")) {
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xsel"))
     expect_true(is_clipr_available)
-  }
+})
+
+test_that("dr_clipr provides informative messages", {
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xclip"))
+    expect_message(dr_clipr(), msg_clipr_available(), fixed = TRUE)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "xsel"))
+    expect_message(dr_clipr(), msg_clipr_available(), fixed = TRUE)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "none"))
+    expect_message(dr_clipr(), msg_no_clipboard(), fixed = TRUE)
+  if (identical(Sys.getenv("TRAVIS_CLIP"), "nodisplay"))
+    expect_message(dr_clipr(), msg_no_display(), fixed = TRUE)
 })
 
 test_that("Unavailable clipboard throws warning", {
